de40fd8d45ab41c24d74edab1297ec2ef5e66c6c
Suvorov Igor <suvorov.igor.90@gmail.com>
2014-06-16 19:24:01 +0400

    Update console.sh

--- console.sh
+++ console.sh
@@ -1,1 +1,3 @@
 mysqldump -p -u it-interview it-interview > it-interview_`date +%d_%b_%Y`.sql  # дампим базу в файл
+
+mysql -uit-interview -pASdasdasdas it-interview < it-interview_12_02_2014.sql  # восстанавливаем базу из файла
