5707da05c2867f107a100909de7059fe44c57d79
Igor Suvorov <suvorov.igor.90@gmail.com>
2015-09-25 11:51:40 +0300

    Update console.sh

--- console.sh
+++ console.sh
@@ -3,6 +3,10 @@ mysqldump -p -u it-interview it-interview > it-interview_`date +%d_%b_%Y`.sql  #
 
 mysql -uit-interview -pASdasdasdas it-interview < it-interview_12_02_2014.sql  # восстанавливаем базу из файла
 
+mongodump -d database123 --out ./dump2
+
+
+
 
 #быстро переименовать / добавить префикс
 rename s/'^'/'MyPrefix'/ *
