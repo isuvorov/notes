b28d111bd5b4ad7e6dca23bcb20c9aab3347edd5
Igor Suvorov <suvorov.igor.90@gmail.com>
2017-02-10 18:05:02 +0400

    Create yarn-add-webpack.sh

---
+++ eslint/yarn-add-webpack.sh
@@ -0,0 +1,11 @@
+yarn add --dev \
+  webpack@1.13.2 \
+  webpack-hot-middleware@2.15.0 \
+  webpack-middleware@1.5.1 \
+  \
+  babel-loader@6.2.4 \
+  css-loader@0.26.1 \
+  eslint-loader@1.6.1 \
+  json-loader@0.5.4 \
+  postcss-loader@1.2.2 \
+  react-hot-loader@3.0.0-beta.6
